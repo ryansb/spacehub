dcadc9a143a65948ee8b2b3d1792fdfef845eca6
Ryan Brown <ryansb@csh.rit.edu>
2013-04-20 19:43:31 -0400

    Add DB initialization.

--- wsgi/corniceapp/corniceapp/__init__.py
+++ wsgi/corniceapp/corniceapp/__init__.py
@@ -2,6 +2,17 @@
 """
 from pyramid.config import Configurator
 
+import os
+
+
+db_name = "spacehub"
+
+db_url = "sqlite:////tmp/test.db"
+
+if os.environ.get("OPENSHIFT_MYSQL_DB_URL", None):
+    db_url = os.environ.get("OPENSHIFT_MYSQL_DB_URL") + db_name
+
+
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
