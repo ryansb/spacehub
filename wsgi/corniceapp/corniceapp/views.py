bfd5f39c92e5da00c5b8764bb7db6d8d91964cb3
Ross Delinger <rossdylan@csh.rit.edu>
2013-04-20 20:11:07 -0400

    stubbed out methods for basic user management

--- wsgi/corniceapp/corniceapp/views.py
+++ wsgi/corniceapp/corniceapp/views.py
@@ -5,6 +5,19 @@ from cornice import Service
 
 hello = Service(name='hello', path='/', description="Simplest app")
 
+users = Service(name='users', path='/users', description="User management api")
+
+@users.get()
+def get_user(request):
+    pass
+
+@users.post()
+def create_user(request):
+    pass
+
+@users.delete()
+def delete_user(request):
+    pass
 
 @hello.get()
 def get_info(request):
