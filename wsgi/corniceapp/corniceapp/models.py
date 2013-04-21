b45c7888826fcf469eef12e306aee3e0319d459a
Ryan Brown <ryansb@csh.rit.edu>
2013-04-21 03:26:16 -0400

    unnest command. Fly birdie

--- wsgi/corniceapp/corniceapp/models.py
+++ wsgi/corniceapp/corniceapp/models.py
@@ -84,7 +84,8 @@ class Repo(_Base):
         return True
 
     def commit_a(self, message):
-        cmd = r'bash -c "cd %s && git add . && git commit -am \"%s\""' % (self.dirname, message)
+        #cmd = r'bash -c "cd %s && git add . && git commit -am \"%s\""' % (self.dirname, message)
+        cmd = r'cd %s && git add . && git commit -am "%s"' % (self.dirname, message)
         print cmd
         subprocess.check_call(cmd.split(' '))
         return True
