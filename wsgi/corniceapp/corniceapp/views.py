2ff19e7f21bd2ce9f3dda24a004b6d4251cb3d0c
Ross Delinger <rossdylan@csh.rit.edu>
2013-04-20 20:27:42 -0400

    pep8 things, and added a method for modifying users

--- wsgi/corniceapp/corniceapp/views.py
+++ wsgi/corniceapp/corniceapp/views.py
@@ -6,8 +6,8 @@ from corniceapp.models import User, Repo
 
 hello = Service(name='hello', path='/', description="Simplest app")
 
+
 users = Service(name='users', path='/users/{username}', description="User management api")
-apikeys = Service(name='apikeys', path='/apikeys', description="Manage API keys")
 
 
 @users.get()
@@ -15,10 +15,18 @@ def get_user(request):
     """Get info on user"""
     pass
 
+
 @users.post()
 def create_user(request):
     """Create a new User"""
     pass
+
+
+@users.put()
+def edit_user(request):
+    """ Edit an existing user """
+    pass
+
 
 @users.delete()
 def delete_user(request):
@@ -26,15 +34,20 @@ def delete_user(request):
     pass
 
 
+apikeys = Service(name='apikeys', path='/apikeys', description="Manage API keys")
+
+
 @apikeys.post()
 def create_key(request):
     """Create a new api key for a user"""
     pass
 
+
 @apikeys.delete()
 def delete_key(request):
     """Delete an api key"""
     pass
+
 
 @hello.get()
 def get_info(request):
