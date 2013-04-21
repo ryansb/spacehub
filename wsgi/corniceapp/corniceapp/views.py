ee96125866913a6c9ef1d2d6836aa3174d3b75b3
Ross Delinger <rossdylan@csh.rit.edu>
2013-04-20 20:19:44 -0400

    added more stubbs for api key management and added some basic doc strings

--- wsgi/corniceapp/corniceapp/views.py
+++ wsgi/corniceapp/corniceapp/views.py
@@ -5,18 +5,34 @@ from cornice import Service
 
 hello = Service(name='hello', path='/', description="Simplest app")
 
-users = Service(name='users', path='/users', description="User management api")
+users = Service(name='users', path='/users/{username}', description="User management api")
+apikeys = Service(name='apikeys', path='/apikeys', description="Manage API keys")
+
 
 @users.get()
 def get_user(request):
+    """Get info on user"""
     pass
 
 @users.post()
 def create_user(request):
+    """Create a new User"""
     pass
 
 @users.delete()
 def delete_user(request):
+    """Delete a user"""
+    pass
+
+
+@apikeys.post()
+def create_key(request):
+    """Create a new api key for a user"""
+    pass
+
+@apikeys.delete()
+def delete_key(request):
+    """Delete an api key"""
     pass
 
 @hello.get()
