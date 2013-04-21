9efa00b044dce0cd4b1c000004fdae626e865d42
Ryan Brown <ryansb@csh.rit.edu>
2013-04-20 20:12:35 -0400

    Add models

--- wsgi/corniceapp/corniceapp/views.py
+++ wsgi/corniceapp/corniceapp/views.py
@@ -1,6 +1,7 @@
 """ Cornice services.
 """
 from cornice import Service
+from corniceapp.models import User, Repo
 
 
 hello = Service(name='hello', path='/', description="Simplest app")
@@ -10,3 +11,27 @@ hello = Service(name='hello', path='/', description="Simplest app")
 def get_info(request):
     """Returns Hello in JSON."""
     return {'Hello': 'World'}
+
+
+repo = Service(name='repo', path='/repo', description="Service to deal with "
+              "the addition/deletion of repositories")
+
+@repo.get()
+def get_repos(request):
+    return Repo.all()
+
+
+@repo.put()
+def put_repo(request):
+    """
+        To edit an existing repository
+    """
+    pass
+
+
+@repo.post()
+def post_repo(request):
+    """
+        To create a new repository
+    """
+    pass
