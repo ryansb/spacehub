646ce287d86adb244809427cbea48ea08fb9b156
Ross Delinger <rossdylan@csh.rit.edu>
2013-04-21 20:29:12 -0400

    fixed a bad db call

--- wsgi/corniceapp/corniceapp/views/oauth.py
+++ wsgi/corniceapp/corniceapp/views/oauth.py
@@ -16,56 +16,56 @@ oauth = Service(name='oauth', path='/oauth', description="Service to deal with G
 @oauth.get()
 def oauth_get_code(request):
 
-	client_id = request.registry.settings["client_id"]
-	client_secret = request.registry.settings["client_secret"]
-	response = oauth_authorize(request)
-	secret = None
-	if DBSession.get(Secret).all():
-		secret = DBSession.get(Secret).all()[0]
-	else:
-		secret = Secret()
-	# do stuff to secret to set info
-	secret.access_token = response.access_token
-	DBSession.add(secret)
-	DBSession.commit()
+    client_id = request.registry.settings["client_id"]
+    client_secret = request.registry.settings["client_secret"]
+    response = oauth_authorize(request)
+    secret = None
+    if DBSession.query(Secret).all().count > 0:
+        secret = DBSession.get(Secret).first()
+    else:
+        secret = Secret()
+    # do stuff to secret to set info
+    secret.access_token = response.access_token
+    DBSession.add(secret)
+    DBSession.commit()
 
-	params = {
-		"title": "Spacehub",
-		"key": secret.public_key
-	}
+    params = {
+        "title": "Spacehub",
+        "key": secret.public_key
+    }
 
-	# add public key to user's acc.
-	requests.post(GITHUB_API_URL + "/user/keys", params=params)
+    # add public key to user's acc.
+    requests.post(GITHUB_API_URL + "/user/keys", params=params)
 
-	raise HTTPTemporaryRedirect(location="/app/index.html#/repos")
+    raise HTTPTemporaryRedirect(location="/app/index.html#/repos")
 
 def oauth_authorize(request):
-	"""
-	Use a previously received auth code or refresh token to get a new
-	access token and refresh token if applicable.
+    """
+    Use a previously received auth code or refresh token to get a new
+    access token and refresh token if applicable.
 
-	"""
+    """
 
-	params = {
-		"client_id": request.registry.settings["client_id"],
-		"client_secret": request.registry.settings["client_secret"],
-		"code": request.params["code"]
-	}
-	headers = {
-		"Accept": "application/json"
-	}
-	return requests.post(GITHUB_TOKEN_URL, params, headers=headers).json()
+    params = {
+        "client_id": request.registry.settings["client_id"],
+        "client_secret": request.registry.settings["client_secret"],
+        "code": request.params["code"]
+    }
+    headers = {
+        "Accept": "application/json"
+    }
+    return requests.post(GITHUB_TOKEN_URL, params, headers=headers).json()
 
 # FIrst
 @oauth.post()
 def outh_redirect(request):
-	scopes = ['repo', 'notifications', 'gist', 'user']
-	params = {
-			"client_id": request.registry.settings["client_id"],
-			# "client_secret": request.registry.settings["client_secret"],
-			"scope": ','.join(scopes),
-			#"state": str(uuid.uuid4())
-	}
+    scopes = ['repo', 'notifications', 'gist', 'user']
+    params = {
+            "client_id": request.registry.settings["client_id"],
+            # "client_secret": request.registry.settings["client_secret"],
+            "scope": ','.join(scopes),
+            #"state": str(uuid.uuid4())
+    }
 
-	raise HTTPSeeOther(location="%s?%s" % (GITHUB_AUTH_URL, urllib.urlencode(params)))
+    raise HTTPSeeOther(location="%s?%s" % (GITHUB_AUTH_URL, urllib.urlencode(params)))
 
